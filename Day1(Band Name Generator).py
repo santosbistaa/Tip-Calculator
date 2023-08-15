9bd191ca2129e91494ecc702bd64e69e447cf94c
santosbistaa <142007698+santosbistaa@users.noreply.github.com>
2023-08-15 17:19:20 +0545

    first commit

---
+++ Day1(Band Name Generator).py
@@ -0,0 +1,4 @@
+print("Welcome To The Band Name Generator")
+city=input("Enter the city that you were born in?\n")
+pet=input("Enter the name of your first pet?\n")
+print("Your band name could be "+city+" "+pet)
